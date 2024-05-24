ffccf5d844706db8a590a538b7765b3a12a9a6bc
Narlotl <fretwellcalifornia@gmail.com>
2024-05-24 06:47:54 -0700

    Change title to ScanGov California

--- scripts/util.js
+++ scripts/util.js
@@ -26,7 +26,7 @@ const getData = async (file) => {
         cache = await caches.open('data');
 
     const cacheTime = await getCacheTime();
-    const commits = await (await fetch('https://api.github.com/repos/civichackingagency/scangov/commits?path=/data&per_page=1&sha=main')).json();
+    const commits = await (await fetch('https://api.github.com/repos/civichackingagency/scangov-california/commits?path=/data&per_page=1&sha=base')).json();
     const updateTime = new Date(commits[0].commit.author.date);
     document.getElementById('updated').innerText = updateTime.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' });
 
